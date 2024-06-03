afed7715caa29c2f60c743f0177a39892d01c8e6
kashubiaksofiia <kashsophia@gmail.com>
2024-06-03 20:54:24 +0300

    design for dashboardPage.vue

--- front_cinema/src/components/others/dashboardPage.vue
+++ front_cinema/src/components/others/dashboardPage.vue
@@ -1,45 +1,59 @@
 <template>
-    <div>
-      
+  <div class="parent-container-5">
+    <div class="table-container">
       <h2>Cinemas</h2>
-    <table>
-      <thead>
-        <tr>
-          <th v-for="(value, key) in cinemas[0]" :key="key"><span  v-if="key !== '__v' && key !== 'photoUrl'">{{ key }}</span></th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="cinema in cinemas" :key="cinema._id">
-          <td v-for="(value, key) in cinema" :key="key" ><span v-if="key !== '__v'">{{ value }}</span></td>
-          <td>
-            <button @click="deleteCinema(cinema._id)">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+          <tr>
+            <th v-for="(value, key) in cinemas[0]" :key="key">
+              <span v-if="key !== '__v' && key !== 'photoUrl'">{{ key }}</span>
+            </th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="cinema in cinemas" :key="cinema._id">
+            <td v-for="(value, key) in cinema" :key="key">
+              <span v-if="key !== '__v'">{{ value }}</span>
+            </td>
+            <td>
+              <button class="btn btn-delete" @click="deleteCinema(cinema._id)">Delete</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
 
-    <h2>Movies</h2>
-    <table>
-      <thead>
-        <tr>
-          <th v-for="(value, key) in movies[0]" :key="key"><span  v-if="key !== '__v' && key !== 'photoUrl'">{{ key }}</span></th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="movie in movies" :key="movie._id">
-          <td v-for="(value, key) in movie" :key="key" ><span v-if="key !== '__v' && key !== 'photoUrl'">{{ value }}</span></td>
-          <td>
-            <router-link :to="{ name: 'UpdateMovie', params: { id: movie._id }}">Update</router-link>
-            <button @click="deleteMovie(movie._id)">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <h2>Movies</h2>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+          <tr>
+            <th v-for="(value, key) in movies[0]" :key="key">
+              <span v-if="key !== '__v' && key !== 'photoUrl'">{{ key }}</span>
+            </th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="movie in movies" :key="movie._id">
+            <td v-for="(value, key) in movie" :key="key">
+              <span v-if="key !== '__v' && key !== 'photoUrl'">{{ value }}</span>
+            </td>
+            <td>
+              <router-link class="btn btn-update" :to="{ name: 'UpdateMovie', params: { id: movie._id }}">Update</router-link>
+              <button class="btn btn-delete" @click="deleteMovie(movie._id)">Delete</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
       <h2>Sessions</h2>
-      <table>
-        <thead>
+      <div class="table-wrapper">
+        <table>
+          <thead>
           <tr>
             <th>ID</th>
             <th>Movie ID</th>
@@ -48,12 +62,12 @@
             <th>Time</th>
             <th>Price</th>
             <th>Discount</th>
-            <th>quantityAvailablePlaces</th>
+            <th>Quantity Available Places</th>
             <th>Actions</th>
           </tr>
-        </thead>
-        <tbody>
-          <tr v-for="session in sessions" :key="session.id">
+          </thead>
+          <tbody>
+          <tr v-for="session in sessions" :key="session._id">
             <td>{{ session._id }}</td>
             <td>{{ session.movieId }}</td>
             <td>{{ session.cinemaId }}</td>
@@ -63,16 +77,18 @@
             <td>{{ session.discount }}</td>
             <td>{{ session.quantityAvailablePlaces }}</td>
             <td>
-            <router-link :to="{ name: 'UpdateSession', params: { id: session._id }}">Update</router-link>
-              <button @click="deleteSession(session._id)">Delete</button>
+              <router-link class="btn btn-update" :to="{ name: 'UpdateSession', params: { id: session._id }}">Update</router-link>
+              <button class="btn btn-delete" @click="deleteSession(session._id)">Delete</button>
             </td>
           </tr>
-        </tbody>
-      </table>
-  
+          </tbody>
+        </table>
+      </div>
+
       <h2>Users</h2>
-      <table>
-        <thead>
+      <div class="table-wrapper">
+        <table>
+          <thead>
           <tr>
             <th>ID</th>
             <th>Name</th>
@@ -80,79 +96,170 @@
             <th>Email</th>
             <th>Actions</th>
           </tr>
-        </thead>
-        <tbody>
-          <tr v-for="user in users" :key="user.id">
+          </thead>
+          <tbody>
+          <tr v-for="user in users" :key="user._id">
             <td>{{ user._id }}</td>
             <td>{{ user.firstName }}</td>
             <td>{{ user.lastName }}</td>
             <td>{{ user.email }}</td>
             <td>
-              <button @click="deleteUser(user._id)">Delete</button>
+              <button class="btn btn-delete" @click="deleteUser(user._id)">Delete</button>
             </td>
           </tr>
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import * as cinemaAPI from '@/services/cinemaAPI'
-  import * as movieAPI from '@/services/movieAPI'
-  import * as sessionAPI from '@/services/sessionAPI'
-  import * as userAPI from '@/services/userAPI'
-  
-  export default {
-    data() {
-      return {
-        cinemas: [],
-        movies: [],
-        sessions: [],
-        users: [],
-      };
+  </div>
+</template>
+
+
+
+<script>
+import * as cinemaAPI from '@/services/cinemaAPI'
+import * as movieAPI from '@/services/movieAPI'
+import * as sessionAPI from '@/services/sessionAPI'
+import * as userAPI from '@/services/userAPI'
+
+export default {
+  data() {
+    return {
+      cinemas: [],
+      movies: [],
+      sessions: [],
+      users: [],
+    };
+  },
+  methods: {
+    async deleteSession(id) {
+      await sessionAPI.deleteSession(id);
     },
-    methods: {
-        async deleteSession(id){
-            await sessionAPI.deleteSession(id);
-        },
-        async deleteUser(id){
-            await userAPI.deleteUser(id);
-        },
-        async deleteCinema(id){
-            await cinemaAPI.deleterCinema(id);
-        },
-        async deleteMovie(id){
-            await movieAPI.deleteMovie(id);
-        }
+    async deleteUser(id) {
+      await userAPI.deleteUser(id);
     },
-    async mounted() {
-      this.cinemas = await cinemaAPI.getAllCinemas();
-      this.movies = await movieAPI.getAllMovies();
-      this.sessions = await sessionAPI.getAllSessions();
-      this.users = await userAPI.getAllUsers();
+    async deleteCinema(id) {
+      await cinemaAPI.deleterCinema(id);
+    },
+    async deleteMovie(id) {
+      await movieAPI.deleteMovie(id);
     }
-  };
-  </script>
-  
-  <style>
-  table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-bottom: 20px;
+  },
+  async mounted() {
+    this.cinemas = await cinemaAPI.getAllCinemas();
+    this.movies = await movieAPI.getAllMovies();
+    this.sessions = await sessionAPI.getAllSessions();
+    this.users = await userAPI.getAllUsers();
   }
-  
-  th, td {
-    border: 1px solid #ddd;
-    padding: 8px;
-  }
-  
-  th {
-    background-color: #f2f2f2;
-    text-align: left;
-  }
-  
-  button {
-    margin-right: 5px;
-  }
-  </style>
-  
+};
+</script>
+
+<style>
+.parent-container-5 {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 20px;
+  overflow-y: auto;
+}
+
+.table-container {
+  width: 100%;
+  max-width: 1400px;
+  margin: 60px;
+  border-radius: 50px;
+  padding: 10px;
+  color: #fff;
+  font-family: 'Cormorant Garamond', serif;
+}
+
+.table-container h2 {
+  margin-bottom: 20px;
+  font-family: Cormorant SC;
+  font-size: 64px;
+  font-weight: 700;
+  line-height: 77.5px;
+  text-align: center;
+  color: rgba(255, 255, 255, 1);
+}
+
+.table-wrapper {
+  overflow-x: auto;
+  margin-bottom: 20px;
+}
+
+.table-wrapper table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table-wrapper th,
+.table-wrapper td {
+  padding: 12px 15px;
+  text-align: center;
+  font-family: Cormorant SC;
+  font-size: 18px;
+  border-bottom: 1px solid #444;
+}
+
+.table-wrapper th {
+  background-color: rgba(54, 26, 114, 1);
+  color: #fff;
+  font-family: Cormorant SC;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+}
+
+.table-wrapper tr:nth-child(even) {
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.table-wrapper tr:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.table-wrapper .btn-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.table-wrapper .btn {
+  padding: 4px 15px;
+  margin: 20px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.2s;
+  font-family: 'Cormorant SC', serif;
+  font-size: 16px;
+}
+
+.table-wrapper .btn-update {
+  background-color: #733E92;
+  border: #FFFFFF 1px solid ;
+  color: white;
+  text-decoration: none;
+}
+
+.table-wrapper .btn-delete {
+  background-color: #501C6F;
+  border: #FFFFFF 1px solid ;
+  color: white;
+}
+
+.table-wrapper .btn-update:hover,
+.table-wrapper .btn-delete:hover {
+  opacity: 0.8;
+  transform: scale(1.05);
+}
+
+
+</style>
